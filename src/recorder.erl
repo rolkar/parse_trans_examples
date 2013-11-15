2c8e9d9abbe4525cdfc68806e2f6cdabb2de8636
Roland Karlsson <roland.karlsson@erlang-solutions.com>
2013-11-15 14:51:27 +0100

    Prettier syntax for exprecs

--- src/recorder.erl
+++ src/recorder.erl
@@ -2,6 +2,10 @@
 
 -compile({parse_transform, exprecs}).
 
+%% Omit this if you want default syntax
+-exprecs_prefix(["#", operation]).
+-exprecs_fname([prefix, "-", record]).
+
 -record(foo, {x, y}).
 
 -export_records([foo]).
--- src/test_recorder.erl
+++ src/test_recorder.erl
@@ -1,6 +1,18 @@
 -module(test_recorder).
 
--export([tst/0]).
+-export([tst/0,
+         tst2/0,
+         tst_default_syntax/0,
+         tst_default_syntax2/0]).
 
 tst() ->
+    7 = recorder:'#get'(x, recorder:'#set'([{x,7}], recorder:'#new'(foo))).
+
+tst2() ->
+    7 = recorder:'#get'(x, recorder:'#set'([{x,7}], recorder:'#new-foo'())).
+
+tst_default_syntax() ->
+    7 = recorder:'#get-'(x, recorder:'#set-'([{x,7}], recorder:'#new-'(foo))).
+
+tst_default_syntax2() ->
     7 = recorder:'#get-'(x, recorder:'#set-'([{x,7}], recorder:'#new-foo'())).
