3993e9073b6ed8d571148ef87f0a2cb31ffbebf5
Roland Karlsson <roland.karlsson@erlang-solutions.com>
2013-11-28 14:55:56 +0100

    Added more recorder tests

--- src/test_recorder.erl
+++ src/test_recorder.erl
@@ -2,8 +2,10 @@
 
 -export([tst/0,
          tst2/0,
+         tst3/0,
          tst_default_syntax/0,
-         tst_default_syntax2/0]).
+         tst_default_syntax2/0,
+         tst_default_syntax3/0]).
 
 tst() ->
     7 = recorder:'#get'(x, recorder:'#set'([{x,7}], recorder:'#new'(foo))).
@@ -11,8 +13,14 @@ tst() ->
 tst2() ->
     7 = recorder:'#get'(x, recorder:'#set'([{x,7}], recorder:'#new-foo'())).
 
+tst3() ->
+    7 = recorder:'#get-foo'(x, recorder:'#set-foo'([{x,7}], recorder:'#new-foo'())).
+
 tst_default_syntax() ->
     7 = recorder:'#get-'(x, recorder:'#set-'([{x,7}], recorder:'#new-'(foo))).
 
 tst_default_syntax2() ->
     7 = recorder:'#get-'(x, recorder:'#set-'([{x,7}], recorder:'#new-foo'())).
+
+tst_default_syntax3() ->
+    7 = recorder:'#get-foo'(x, recorder:'#set-foo'([{x,7}], recorder:'#new-foo'())).
